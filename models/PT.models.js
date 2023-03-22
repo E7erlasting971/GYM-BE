a9bf1022309249d164240f873deb65bbc4bcbc2b
Ngô Thế Bách <96104099+NgoTheBach@users.noreply.github.com>
2023-03-22 23:11:46 +0700

    User and PT

---
+++ models/PT.models.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const PTSchema = new mongoose.Schema({
+  TenPT: {
+    type: String,
+    required: true,
+  },
+  SoDienThoaiPT: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  EmailPT: {
+    type: String,
+    required: true,
+  },
+  ImagePT: {
+    type: String,
+    required: true,
+  }
+
+},{ versionKey: false } ); 
+// loại bỏ thuộc tính __v vì khi mình POST lên thì nó tạo thêm giá trị " __v " vì cái này là tính năng của mongodb
+
+module.exports = mongoose.model("PT", PTSchema);
